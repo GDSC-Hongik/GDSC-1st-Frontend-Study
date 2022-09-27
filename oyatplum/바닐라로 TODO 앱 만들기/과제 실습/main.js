79e9b7efc19aaccdad5db9564dcd922a149fa4f8
yeji <oyatpeach@naver.com>
2022-09-27 14:56:52 +0900

    [Feat: add count]

--- oyatplum/바닐라로 TODO 앱 만들기/과제 실습/main.js
+++ oyatplum/바닐라로 TODO 앱 만들기/과제 실습/main.js
@@ -15,3 +15,33 @@ const addTodoList = () => {
     //renderTodoItem();
   }
 };
+// isDone의 상태를 반대로 바꿔 준다
+const toggleItem = (e) => {
+  const itemObject = itemList.find(
+    (inputObject) => inputObject.text === e.target.innerText
+  );
+  itemObject.isDone = !itemObject.isDone;
+  localStorage.setItem("itemList", JSON.stringify(itemList)); // 로컬 스토리지 갱신
+  //renderTodoItem();
+};
+
+// 로컬 스토리지에서 값을 삭제한다
+const removeItem = (e) => {
+  const filteredList = itemList.filter(
+    (inputObject) =>
+      inputObject.text !== e.target.parentNode.innerText.slice(0, -2)
+  );
+  localStorage.setItem("itemList", JSON.stringify(filteredList)); // 로컬 스토리지 갱신
+  //renderTodoItem();
+};
+
+// todo, done의 수 카운트
+const countItem = () => {
+  const todoCount = document.getElementById("todo-count");
+  todoCount.innerText = ` (${itemList.filter((item) => !item.isDone).length})`;
+
+  const doneCount = document.getElementById("done-count");
+  doneCount.innerText = ` (${itemList.filter((item) => item.isDone).length})`;
+};
+
+//window.onload = renderTodoItem();
