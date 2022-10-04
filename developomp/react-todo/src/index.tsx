241cb2b8bdc7aa8c82dc3810da7dafdb2b9b76f9
developomp <developomp@gmail.com>
2022-10-04 23:14:17 +0900

    create week 2 project skeleton

---
+++ developomp/react-todo/src/index.tsx
@@ -0,0 +1,11 @@
+import "./index.css"
+
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App"
+
+ReactDOM.createRoot(document.body).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+)
