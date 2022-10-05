bece0feadf1c3f1aaeb06f8af76a27a8a1142afe
ohhamma <81293930+ohhamma@users.noreply.github.com>
2022-10-05 21:47:28 +0900

    [Feat: Title 컴포넌트 추가]

--- ohhamma/react-todo/src/Container.js
+++ ohhamma/react-todo/src/Container.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import Title from './Title'
 
 const Container = () => {
   return (
-    <>
-      <div className='container'></div>
-    </>
+    <div className='container'>
+      <Title title='⛧ 투두리스트 ⛧' />
+    </div>
   )
 }
 
---
+++ ohhamma/react-todo/src/Title.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Title = ({ title }) => {
+  return (
+    <>
+      <div className='title'>{ title }</div>
+    </>
+  )
+}
+
+export default Title
