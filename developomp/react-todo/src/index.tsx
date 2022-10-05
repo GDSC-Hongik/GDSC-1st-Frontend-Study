1fdd879c8cb01d8d9e47bd52c92a3eaf80905891
developomp <developomp@gmail.com>
2022-10-05 23:14:27 +0900

    fix dispatch function being called multiple times

--- developomp/react-todo/src/index.tsx
+++ developomp/react-todo/src/index.tsx
@@ -8,14 +8,14 @@ import { GlobalStore } from "./globalContext"
 
 import App from "./App"
 
+// 않이 대체 왜인지는 모르겠지만 strict모드를 사용하면 global state를 사용할 때 버그가 있다네요
+// https://github.com/facebook/react/issues/16295#issuecomment-606843399
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
-		<GlobalStore>
-			<BrowserRouter>
-				<HelmetProvider>
-					<App />
-				</HelmetProvider>
-			</BrowserRouter>
-		</GlobalStore>
-	</React.StrictMode>
+	<GlobalStore>
+		<BrowserRouter>
+			<HelmetProvider>
+				<App />
+			</HelmetProvider>
+		</BrowserRouter>
+	</GlobalStore>
 )
