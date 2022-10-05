f212a803904e8aea0dfc6a3fc83a621bf21b12ae
developomp <developomp@gmail.com>
2022-10-06 00:22:02 +0900

    implement List

--- developomp/react-todo/src/components/List/List.tsx
+++ developomp/react-todo/src/components/List/List.tsx
@@ -3,10 +3,23 @@ import "./List.css"
 import { useContext } from "react"
 import { globalContext } from "../../globalContext"
 
+import AddItemButton from "./AddItemButton"
+import ToDoItem from "./ToDoItem"
+
 function List() {
 	const { globalState } = useContext(globalContext)
 
-	return <div className="list">{globalState.todo.toString()}</div>
+	return (
+		<div className="list">
+			<h2>TODO</h2>
+
+			{Object.keys(globalState.todo).map((key) => (
+				<ToDoItem key={key} todoKey={key} content={globalState.todo[key]} />
+			))}
+
+			<AddItemButton />
+		</div>
+	)
 }
 
 export default List
