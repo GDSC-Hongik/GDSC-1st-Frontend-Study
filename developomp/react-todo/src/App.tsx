4e8f7e95fba45afae27c2e5a0091d5a8ace246d9
developomp <developomp@gmail.com>
2022-10-05 22:54:15 +0900

    move page title control from index.html to react

--- developomp/react-todo/src/App.tsx
+++ developomp/react-todo/src/App.tsx
@@ -1,5 +1,7 @@
 import "./App.css"
 
+import { Helmet } from "react-helmet-async"
+
 import Header from "./components/Header"
 import Toolbar from "./components/Toolbar"
 import ListContainer from "./components/List/ListContainer"
@@ -7,6 +9,10 @@ import ListContainer from "./components/List/ListContainer"
 function App() {
 	return (
 		<>
+			<Helmet>
+				<title>TODO</title>
+			</Helmet>
+
 			<Header />
 			<Toolbar />
 			<ListContainer />
