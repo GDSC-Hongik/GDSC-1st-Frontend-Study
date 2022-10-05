7d4ad0c216ae3810212d9f5beb4acd978cddc2be
developomp <developomp@gmail.com>
2022-10-05 21:33:57 +0900

    add components to App.tsx

--- developomp/react-todo/src/App.tsx
+++ developomp/react-todo/src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css"
 
+import Header from "./components/Header"
+import Toolbar from "./components/Toolbar"
+import ListContainer from "./components/List/ListContainer"
+
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">app</header>
-		</div>
+		<>
+			<Header />
+			<Toolbar />
+			<ListContainer />
+		</>
 	)
 }
 
