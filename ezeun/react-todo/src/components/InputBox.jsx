4a3a642b7ea03a48c0af840c98135d3592b5c03e
ezeun <consistency1231@gmail.com>
2022-10-07 01:22:41 +0900

    [Feat: input 가져오기]

--- ezeun/react-todo/src/components/InputBox.jsx
+++ ezeun/react-todo/src/components/InputBox.jsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
-const InputBox = () => (
-  <div className="todoapp__inputbox">
-    {/* 아이템 내용 입력 input */}
-    <input
-      type="text"
-      name="todoItem"
-      placeholder="할 일을 입력해주세요"
-      className="todoapp__inputbox-inp"
-    />
-    {/* 입력 후 아이템 추가 버튼 */}
-    <button type="submit" className="todoapp__inputbox-add-btn">
-      추가
-    </button>
-  </div>
-);
+const InputBox = () => {
+
+    const [text, setText] = useState('');
+    const inputRef = useRef(null);
+
+    const onChangeInput = (e) => {
+        setText(e.target.value);
+    };
+    
+    const onClickAddButton = () => { 
+        setText(''); 
+        inputRef.current.focus(); 
+    };
+    
+    return (
+        <div className="todoapp__inputbox">
+            {/* 아이템 내용 입력 input */}
+            <input
+                type="text"
+                name="todoItem"
+                value={text}
+                ref={inputRef}
+                placeholder="할 일을 입력해주세요"
+                className="todoapp__inputbox-inp"
+                onChange={onChangeInput}
+            />
+            {/* 입력 후 아이템 추가 버튼 */}
+            <button
+                type="submit"
+                className="todoapp__inputbox-add-btn"
+                onClick={onClickAddButton}
+            >
+                추가
+            </button>
+        </div>
+    );
+};
 
 export default InputBox;
--- ezeun/react-todo/src/pages/Home.jsx
+++ ezeun/react-todo/src/pages/Home.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputBox from '../components/InputBox';
 import ToDoItemList from '../components/ToDoItemList';
 
-const Home = () => (
-  <div className="homepage__container">
-    {/* ToDo Item을 추가할 수 있는 input 박스 */}
-    <InputBox />
+const Home = () => {
+  const [todoList, setTodoList] = useState([]);
 
-    {/* 할 일 Item 리스트 */}
-    <ToDoItemList />
+  return (
+    <div className="homepage__container">
+      {/* ToDo Item을 추가할 수 있는 input 박스 */}
+      <InputBox todoList={todoList} setTodoList={setTodoList} />
 
-    {/* 완료한 Item 리스트 */}
-    <ToDoItemList />
-  </div>
-);
+      {/* 할 일 Item 리스트 */}
+      <ToDoItemList />
+
+      {/* 완료한 Item 리스트 */}
+      <ToDoItemList />
+    </div>
+  );
+};
 
 export default Home;
