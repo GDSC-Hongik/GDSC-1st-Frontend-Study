92d8517460783d7840f500651dd3550f7f9ffe51
ezeun <consistency1231@gmail.com>
2022-10-06 19:30:21 +0900

    start react

---
+++ ezeun/react-todo/src/App.js
@@ -0,0 +1,13 @@
+import Button from "./Button";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <h2 className={styles.title}>Leeju's TODO List</h2>
+      <Button text={"cc"} />
+    </div>
+  );
+}
+
+export default App;
