cf24f40c66effbcd60cca9f98ac6853514ce5ce9
ezeun <consistency1231@gmail.com>
2022-10-07 17:25:42 +0900

    [Fix: 할 일 이름 설정]

--- ezeun/react-todo/src/components/ToDoItem.jsx
+++ ezeun/react-todo/src/components/ToDoItem.jsx
@@ -6,7 +6,7 @@ const ToDoItem = ({ todoItem, todoList, setTodoList }) => (
     {/* 아이템 완료 체크 / 체크 해제를 위한 체크박스 */}
     <input type="checkbox" className="todoapp__item-checkbox" />
     {/* 아이템 내용 */}
-    <span className="todoapp__item-ctx">ToDo Item</span>
+    <span className="todoapp__item-ctx">{todoItem.text}</span>
     {/* 수정 버튼 */}
     <button type="button" className="todoapp__item-edit-btn">
       ✏
