bf26dbbd1208122caf54aa02acad8a0a8d1cbc40
developomp <developomp@gmail.com>
2022-11-02 13:58:42 +0900

    fix: invisible list title

--- developomp/react-todo/src/components/List/List.tsx
+++ developomp/react-todo/src/components/List/List.tsx
@@ -5,6 +5,8 @@ import tw from "twin.macro"
 import AddItemButton from "./AddItemButton"
 import ToDoItem from "./ToDoItem"
 
+const ListTitle = tw.h2`text-white font-bold text-2xl mb-3`
+
 const ListComponent = tw.div`
 	flex flex-col w-80 rounded-xl pt-3 px-5
 	whitespace-nowrap gap-2 overflow-y-scroll
@@ -16,7 +18,7 @@ function List() {
 
 	return (
 		<ListComponent>
-			<h2>TODO</h2>
+			<ListTitle>TODO</ListTitle>
 
 			{Object.keys(globalState.todo).map((key) => (
 				<ToDoItem key={key} todoKey={key} content={globalState.todo[key]} />
