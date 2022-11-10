2eac1f22fde373125c2d698ec9d264b42f9a303d
developomp <developomp@gmail.com>
2022-11-11 03:00:04 +0900

    add temperature indication and settings page

--- developomp/react-todo/src/globalContext.tsx
+++ developomp/react-todo/src/globalContext.tsx
@@ -1,18 +1,30 @@
-import type { Dispatch, ReactNode, ReactElement } from "react"
+import { Dispatch, ReactNode, ReactElement, useState } from "react"
 
-import { createContext, useReducer } from "react"
+import { createContext } from "react"
 import { nanoid } from "nanoid"
+import axios from "axios"
 
 /**
- * Type definitions
+ * Type and constant definitions
  */
 
 export interface TodoList {
 	[key: string]: string
 }
 
+interface WeatherData {
+	hourly: {
+		temperature_2m: number[]
+	}
+	hourly_units: {
+		temperature_2m: string
+	}
+}
+
 export interface IGlobalState {
 	todo: TodoList
+	pageTitle: string
+	weatherData: WeatherData
 }
 
 export type GlobalAction =
@@ -27,6 +39,13 @@ export type GlobalAction =
 			type: ActionsEnum.UPDATE_TODO
 			payload: { key: string; content: string }
 	  }
+	| {
+			type: ActionsEnum.UPDATE_WEATHER
+	  }
+	| {
+			type: ActionsEnum.UPDATE_PATHNAME
+			payload: string
+	  }
 
 export interface IGlobalContext {
 	globalState: IGlobalState
@@ -37,10 +56,17 @@ export enum ActionsEnum {
 	ADD_TODO,
 	REMOVE_TODO,
 	UPDATE_TODO,
+	UPDATE_WEATHER,
+	UPDATE_PATHNAME,
 }
 
 export enum LocalStorageKey {
 	TODO = "todo",
+}
+
+const pathnameToTitle = {
+	"/": "Home",
+	"/settings": "Settings",
 }
 
 /**
@@ -54,11 +80,20 @@ function saveToLocalStorage(todo: any) {
 const defaultState: IGlobalState = {
 	todo:
 		JSON.parse(window.localStorage.getItem(LocalStorageKey.TODO) as any) || {},
+	pageTitle: "",
+	weatherData: {
+		hourly: {
+			temperature_2m: [0],
+		},
+		hourly_units: {
+			temperature_2m: "°C",
+		},
+	},
 }
 
 export const globalContext = createContext({} as IGlobalContext)
 
-function reducer(state = defaultState, action: GlobalAction): IGlobalState {
+async function reducer(state = defaultState, action: GlobalAction) {
 	switch (action.type) {
 		case ActionsEnum.ADD_TODO:
 			state.todo[nanoid()] = ""
@@ -73,6 +108,24 @@ function reducer(state = defaultState, action: GlobalAction): IGlobalState {
 			state.todo[key] = content
 			break
 
+		case ActionsEnum.UPDATE_WEATHER:
+			try {
+				// https://open-meteo.com/en/docs#api-documentation
+				const response = await axios.get(
+					"https://api.open-meteo.com/v1/forecast?latitude=37.5139&longitude=126.9828&hourly=temperature_2m"
+				)
+				state.weatherData = response.data as unknown as WeatherData
+			} catch (error) {}
+			break
+
+		case ActionsEnum.UPDATE_PATHNAME:
+			try {
+				state.pageTitle = (pathnameToTitle as any)[action.payload]
+			} catch (error) {
+				state.pageTitle = ""
+			}
+			break
+
 		default:
 			break
 	}
@@ -82,8 +135,16 @@ function reducer(state = defaultState, action: GlobalAction): IGlobalState {
 	return { ...state }
 }
 
+function useAsyncReducer(reducer: any, initState: any) {
+	const [state, setState] = useState(initState)
+	const dispatchState = async (action: any) =>
+		setState(await reducer(state, action))
+
+	return [state, dispatchState]
+}
+
 export function GlobalStore(props: { children: ReactNode }): ReactElement {
-	const [globalState, dispatch] = useReducer(reducer, defaultState)
+	const [globalState, dispatch] = useAsyncReducer(reducer, defaultState)
 
 	return (
 		<globalContext.Provider value={{ globalState, dispatch }}>
