a9e2fe0af09cd2093b86b6015e653ed14e6b2534
developomp <developomp@gmail.com>
2022-11-11 03:00:04 +0900

    add temperature indication and settings page

--- developomp/react-todo/src/components/Header.tsx
+++ developomp/react-todo/src/components/Header.tsx
@@ -1,9 +1,33 @@
 import tw from "twin.macro"
+import { useContext } from "react"
+import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faGear, faHome } from "@fortawesome/free-solid-svg-icons"
 
-const HeaderComponent = tw.header`flex w-full h-28 items-center px-8 bg-gray-700 text-gray-400 font-bold text-3xl`
+import { globalContext } from "../globalContext"
+
+const HeaderComponent = tw.header`flex w-full h-28 items-center justify-between px-8 bg-gray-700 text-gray-400 font-bold text-3xl`
+const NavIconContainer = tw.div`flex gap-3 `
+const NavLink = tw(Link)`hover:text-zinc-100`
 
 function Header() {
-	return <HeaderComponent>pomp's todo</HeaderComponent>
+	const { globalState } = useContext(globalContext)
+
+	return (
+		<HeaderComponent>
+			pomp's todo | {globalState.pageTitle}
+			<NavIconContainer>
+				{globalState.weatherData.hourly.temperature_2m.at(-1) || ""}
+				{globalState.weatherData.hourly_units.temperature_2m}
+				<NavLink to="/">
+					<FontAwesomeIcon icon={faHome} />
+				</NavLink>
+				<NavLink to="/settings">
+					<FontAwesomeIcon icon={faGear} />
+				</NavLink>
+			</NavIconContainer>
+		</HeaderComponent>
+	)
 }
 
 export default Header
