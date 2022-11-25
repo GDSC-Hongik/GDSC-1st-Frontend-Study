2ba47ee844ae5db80ba47667219a67d27a100ffa
Eugene <eugene02876@gmail.com>
2022-11-25 18:31:28 +0900

    [feat] : diary toggle 기능 구현

--- eugene028/react-todo/eugene-todo/src/stores/Atom.js
+++ eugene028/react-todo/eugene-todo/src/stores/Atom.js
@@ -1,17 +1,26 @@
 import { atom } from 'recoil';
-//var defaultDate = todayDate.toLocaleString().slice(0,13).replace(/ /g,"");
 
 export const dateState = atom({
     key : 'dateState',
-    default : new Date() //전역상태의 초기
-}); //date를 전역값으로 관리
+    default : new Date() 
+}); 
 
-export const todoState = atom({ //itom으로 저장. date값도 저장해야할까?
+export const todoState = atom({ 
     key: 'todoState',
     default: [],
   });
 
 export const todoId = atom({
     key : "todoId",
+    default : 0,
+})
+
+export const diaryState = atom({
+    key : "diaryState",
+    default : [],
+})
+
+export const diaryId = atom({
+    key : "diaryId",
     default : 0,
 })
